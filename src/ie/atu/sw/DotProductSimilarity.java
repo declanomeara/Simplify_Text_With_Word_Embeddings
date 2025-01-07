7784ee4da6e87c3ee1a803002138f625144a6f42
Declan O'Meara <declanomeara@yahoo.co.uk>
2025-01-07 19:24:39 +0000

    implemented calculation of similarity cosine, dotproduct and euclideanDistance

--- src/ie/atu/sw/DotProductSimilarity.java
+++ src/ie/atu/sw/DotProductSimilarity.java
@@ -3,9 +3,14 @@ package ie.atu.sw;
 public class DotProductSimilarity implements SimilarityMeasure {
 
 	@Override
-	public double calculate(float[] vector1, float[] vector2) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public double calculate(float[] vector1, float[] vector2) {
+        double dotProduct = 0.0;
+        
+        for (int i = 0; i < vector1.length; i++) {
+            dotProduct += vector1[i] * vector2[i];
+        }
+        
+        return dotProduct;
+    }
 
 }
--- src/ie/atu/sw/EuclideanDistanceSimilarity.java
+++ src/ie/atu/sw/EuclideanDistanceSimilarity.java
@@ -2,10 +2,14 @@ package ie.atu.sw;
 
 public class EuclideanDistanceSimilarity implements SimilarityMeasure {
 
-	@Override
-	public double calculate(float[] vector1, float[] vector2) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	 @Override
+	    public double calculate(float[] vector1, float[] vector2) {
+	        double sum = 0.0;
+	        for (int i = 0; i < vector1.length; i++) {
+	            sum += Math.pow(vector1[i] - vector2[i], 2);
+	        }
+	        
+	        return Math.sqrt(sum);// Return negative for consistency (higher is better)
+	    }
 
 }
